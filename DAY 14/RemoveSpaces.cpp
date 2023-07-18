ab23a43bc0d8a1bee8afc25a37f5b3c017d93ccb
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-18 16:15:54 +0530

    DSA DAY 14

---
+++ DAY 14/RemoveSpaces.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+string RemoveSpace(string s){
+    string temp="";
+    for(int i=0;i<s.length();i++){
+        if(s[i]==' '){
+        temp.push_back('@');
+        temp.push_back('4');
+        temp.push_back('0');
+        }
+        else{
+  temp.push_back(s[i]);
+        }
+    }
+    return temp;
+}
+int main(){
+    string s="My name is GARO";
+    // cout<<"ENTER THE STRING "<<endl;
+    // cin>>s;
+    // cout<<"ENTERRED STRINGN IS :- "<<endl;
+    // cout<<s<<endl;
+    cout<<"REMOVING SPACING AND REPLACING IT WITH @40 "<<RemoveSpace(s)<<endl;
+    // cout<<RemoveSpace(s)<<endl;
+    return 0;
+}
