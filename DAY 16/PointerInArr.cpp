8b9099644a7b4ddb8ed96d4940771170cfb05664
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-20 14:15:59 +0530

    DSA DAY 16

--- DAY 16/PointerInArr.cpp
+++ DAY 16/PointerInArr.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main(){
-    int arr[4]={2,4,6,10};
-    cout<<"LOCATION OR ADDRESS OF THE FIRST ELEMENT IN AN ARRAY "<<arr<<endl;
-    cout<<"LOCATION OF ANY OTHER ELEMENT CAN  BE "<<&arr[1]<<endl;
-    return 0;
+    // int arr[4]={2,4,6,10};
+    // cout<<"LOCATION OR ADDRESS OF THE FIRST ELEMENT IN AN ARRAY "<<arr<<endl;
+    // cout<<"LOCATION OF ANY OTHER ELEMENT CAN  BE "<<&arr[1]<<endl;
+    //
+    int temp[10];
+    cout<<sizeof(temp)<<endl;
+int *ptr=&temp[0];
+    cout<<sizeof(ptr)<<endl;
+    
+     return 0;
+
 }
