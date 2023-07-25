6a75f3cfe4c431696e7f5a86bbdb7d69ab6d8e12
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-25 23:46:46 +0530

    DSA DAY 21

---
+++ DAY 21/OOPS-5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class Person{
+public:
+int a;
+};
+int main(){
+    //DYNAMIC ALLOCATION
+    Person *p=new Person;
+    (*p).a=10;
+    cout<<(*p).a<<endl;
+    //OR
+    cout<<p->a<<endl;
+    return 0;
+}
