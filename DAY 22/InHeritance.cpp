ced6ff18bc15349b47167fad888e6e69e64c9820
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-26 23:06:58 +0530

    DSA DAY 21

---
+++ DAY 22/InHeritance.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Human{
+    public:
+    int height;
+    int weight;
+    int age;
+int getAge(){
+    return this->age;
+}
+void setWeight(int w){
+    this->weight=w;
+}
+int getWeight(){
+    return weight;
+}
+void setHeight(int h){
+this->height=h;
+}
+int getHeight(){
+    return height;
+}
+};
+class Male:public Human{
+    public:
+    string color;
+    void sleep(){
+        cout<<"MALE SLEEPING "<<endl;
+    }
+};
+int main(){
+ Male m;
+ m.age=20;
+//  m.setWeight(93);
+//  m.setHeight(6);
+ m.color="BLACK";
+ cout<<m.getAge()<<endl;
+ cout<<m.getWeight()<<endl;
+ cout<<m.getHeight()<<endl;
+ cout<<m.color<<endl;
+ cout<<m.sleep()<<endl;
+    return 0;
+}
