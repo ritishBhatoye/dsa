ecc20257514400e4e98feaa1f267dd35d5e549db
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-26 12:52:16 +0530

    DSA DAY 21

--- OOPS-10.cpp
+++ OOPS-10.cpp
@@ -1,3 +1,4 @@
+//COPY ASSIGNMENT OPERATOR
 #include<iostream>
 using namespace std;
 class person{
@@ -5,12 +6,26 @@ class person{
     int health;
     string name;
     char level;
-    person(int health,string name,char level){
+    person(string name,char level,int health){
         this->health=health;
         this->name=name;
         this->level=level;
     }
+    void print(){
+        cout<<"NAME IS : "<<name<<endl;
+        cout<<"HEALTH IS : "<<health<<endl;
+        cout<<"LEVEL IS : "<<level<<endl;
+    }
 };
 int main(){
+    person p("RITISH",'H',86);
+    person q("RAHUL",'L',95);
+    p.print();
+    cout<<"ANOTHER OBJECT "<<endl;
+    q.print();
+    //COPYING ALL THE VALUES OF MEMBER FROM THE Q OBJECT TO P OBJECT
+    p=q;
+    p.print();
+    q.print();
     return 0;
 }
