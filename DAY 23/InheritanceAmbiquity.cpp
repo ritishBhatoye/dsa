74fa1106658436c3015e8ecefd7d74d5c6238095
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-28 15:47:03 +0530

    DSA DAY 23

---
+++ DAY 23/InheritanceAmbiquity.cpp
@@ -0,0 +1,24 @@
+//IF SAME FUNCTION ARE THERE 
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    void func(){
+        cout<<"I AM A "<<endl;
+    }
+};
+class B{
+    public:
+void func(){
+    cout<<"I AM B "<<endl;
+}
+};
+class C:public A,public B{
+
+};
+int main(){
+C c;
+c.A::func();
+c.B::func();
+    return 0;
+}
