b64f401829dee365646fed03ed076d4d5b376d36
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-28 22:56:40 +0530

    DSA DAY 23

---
+++ DAY 23/FunctionOverriding.cpp
@@ -0,0 +1,22 @@
+//RUNTIME POLYMORPHISM :- METHOD OVERRIDING
+#include<iostream>
+using namespace std;
+class A{
+public:
+  void speak(){
+    cout<<"A IS SPEAKING "<<endl;
+  }
+};
+class B:public A{
+    public:
+    void speak(){
+        cout<<"B IS SPEAKING "<<endl;
+    }
+};
+int main(){
+    A a;
+    B b;
+    b.speak();
+    a.speak();
+    return 0;
+}
