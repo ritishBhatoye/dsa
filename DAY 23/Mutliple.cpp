b9e7ebcf4bfeadca970394d25d6fe848653e145e
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-28 14:02:16 +0530

    DSA DAY 23

---
+++ DAY 23/Mutliple.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Human{
+    public:
+    int height;
+    int weight;
+};
+class Person{
+    public:
+    string quality;
+
+};
+class Male:public Human,public Person{
+public:
+string prof;
+string color;
+};
+int main(){
+    Male m;
+    m.height=6;
+    m.weight=90;
+    m.quality="GOOD";
+    m.prof="SDE ENGINEER ";
+    m.color="BROWN";
+    cout<<"PROFESSION : "<<m.prof<<endl;
+    cout<<"HEIGHT     : "<<m.height<<endl;
+    cout<<"WEIGHT     : "<<m.weight<<endl;
+    cout<<"QUALITY    : "<<m.quality<<endl;
+    cout<<"COLOR      : "<<m.color<<endl;
+    return 0;
+}
