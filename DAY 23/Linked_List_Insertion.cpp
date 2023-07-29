deafab91931419027d0b003218fc9385e338ea11
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-29 20:16:34 +0530

    DSA DAY 23

--- DAY 23/Linked_List_Insertion.cpp
+++ DAY 23/Linked_List_Insertion.cpp
@@ -4,18 +4,25 @@ class Node{
     public:
     int data;
     Node* next;
-    
+    //CONSTRUCTOR
     Node(int data){
         this->data=data;
         this->next=NULL;
     }
 };
-void InsertAtHead(Node* &head,int d){
-    //new node create
-    Node* temp=new Node()
-
-}
-
+ void InsertAtHead(Node* &head,int d){
+         //new node create
+     Node* temp=new Node(d);
+     temp->next=head;
+     head=temp;
+ }
+ void print(node* &head){
+    
+ }
 int main(){
-    return 0;
+    Node* node1=new Node(10);
+    cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
+
+        return 0;
 }
