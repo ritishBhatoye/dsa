f595e1a9b77c73ea7888d51f4bbb0645bb98aeb6
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-29 19:45:08 +0530

    DSA DAY 2023

---
+++ DAY 23/Linked_List_Insertion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+void InsertAtHead(Node* &head,int d){
+    //new node create
+    Node* temp=new Node()
+
+}
+
+int main(){
+    return 0;
+}
