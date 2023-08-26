d649808364fa625fa3f21384fadad7dbca3f5ae0
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-26 11:09:39 +0530

    DSA

---
+++ DAY 24/PracLinkedList3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node* next;
+Node(int data){
+    this->data=data;
+    this->next=NULL;
+}
+};
+
+void 
+
+int main(){
+    return 0;
+}
