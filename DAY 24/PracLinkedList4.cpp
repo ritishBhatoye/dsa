7a056f9010db029ebf6695aa828dbb355cacffbd
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-27 08:59:30 +0530

    DSA

--- DAY 24/PracLinkedList4.cpp
+++ DAY 24/PracLinkedList4.cpp
@@ -11,12 +11,19 @@ class Node{
     this->next=NULL;
     }
 };
+void InsertAtTail(Node* &tail,int d){
+    Node* temp=new Node(d);
+    tail->next=temp;
+    tail=temp;
 
+}
 void InsertAtHead(Node* &head,int d){
 Node* temp=new Node(d);
 temp->next= head;
 head=temp;
 }
+
+
 
 void print(Node* &head){
     Node* temp=head;
@@ -31,8 +38,13 @@ void print(Node* &head){
 int main(){
     Node* n1=new Node(10);
     Node *head=n1;
+    Node *tail=n1;
     print(head);
-    InsertAtHead(head,20);
+    InsertAtTail(tail,20);
+    print(head);
+    InsertAtTail(tail,30);
+    print(head);
+    InsertAtTail(tail,40);
     print(head);
 
     return 0;
