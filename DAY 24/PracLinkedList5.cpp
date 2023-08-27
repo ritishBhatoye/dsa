976937a86dfce2cb73de9f201c53f6b612db4b8f
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-27 10:27:11 +0530

    DSA

---
+++ DAY 24/PracLinkedList5.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+void InsertAtHead(Node* &head,int d){
+    Node* temp=new Node(d);
+    temp->next=head;
+    head=temp;
+}
+void print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void InsertAtTail(Node* &tail,int d){
+    Node* temp=new Node(d);
+    tail->next=temp;
+    tail=temp;
+}
+void 
+int main(){
+    Node* n1=new Node(10);
+    Node* head=n1;
+    Node* tail=n1;
+    //InsertAtHead(head,20);
+    print(head);
+    InsertAtTail(tail,20);
+    print(head);
+    InsertAtTail(tail,30);
+    print(head);
+    return 0;
+}
