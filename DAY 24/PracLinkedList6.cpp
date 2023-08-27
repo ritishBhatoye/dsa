521398c40366f54b6bf9dfab2233ada6f5bd0e7c
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-27 12:31:35 +0530

    DSA

--- DAY 24/PracLinkedList6.cpp
+++ DAY 24/PracLinkedList6.cpp
@@ -1,7 +1,38 @@
 #include<iostream>
 using namespace std;
 //DELETION
-
+class Node{
+public:
+int data;
+Node* next;
+Node(int data){
+    this->data=data;
+    this->next=NULL;
+}
+};
+void InsertAtHead(Node* &head,int d){
+Node* temp=new Node(d);
+temp->next=head;
+head=temp;
+}
+void InsertAtTail(Node* &tail,int d){
+    Node* temp=new Node(d);
+    tail->next=temp;
+    tail=temp;
+}
+void print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
 int main(){
+    Node* n1=new Node(10);
+    Node* head=n1;
+    Node* tail=n1;
+    InsertAtTail(tail,20);
+    print(head);
     return 0;
 }
