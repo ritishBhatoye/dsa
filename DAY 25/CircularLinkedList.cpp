2b1d9d525b393c6c00d4777b4a5aa571db792f68
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-28 21:21:57 +0530

    DSA

--- DAY 25/CircularLinkedList.cpp
+++ DAY 25/CircularLinkedList.cpp
@@ -19,10 +19,47 @@ class Node{
     }
 };
 
-void insertAtHead(Node* &head,int d){
+void insertNode(Node* &tail,int element,int d){
+//assuming that the element is present in the linked list
+//EMPTY LIST IF TAIL==NULL
+if(tail==NULL){
+Node* newNode=new Node(d);
+tail=newNode;
+newNode->next=newNode;
+}
+//NON-empty list
+//ASSUMING THAT ELEMENT IS PRESENT IN THE LIST
+else{
+ Node* curr=tail;
+ 
+ while(curr->data!=element){
+    curr=curr->next;
+ }
+
+ //ELEMENT FOUND
+ Node* temp=new Node(d);
+
+ temp->next=curr->next;
+ curr->next=temp;
 
 }
-void insertAtTail(Node)
+}
+void print(Node* tail)
+{
+Node* temp=tail;
+cout<<tail->data<<" ";
+
+
+while(tail->next!=temp ){
+    cout<<tail->data<<" ";
+    tail=tail->next;
+}
+cout<<" ";
+}
 int main(){
+    Node* tail=NULL;
+    //Empty list me insert krre hai
+    insertNode(tail,10,3);
+    print(tail);
     return 0;
 }
