364ceadfb7222867ef0bb10b7e4f95346bb64b0b
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-28 14:24:39 +0530

    DSA

---
+++ DAY 25/PracDoublyLinkedList.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+   Node(int d){
+    this->data=d;
+    this->next=NULL;
+   }
+}
+
+
+int main(){
+    return 0;
+}
