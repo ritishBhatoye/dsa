320390232a10fa649862d319b36f247d457441a8
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-28 17:01:09 +0530

    DSA

---
+++ DAY 25/CircularLinkedList.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int d){
+        this->data=d;
+        this->next=NULL;
+    }
+    ~Node(){
+        int value=this->data;
+        if(this->next!=NULL){
+            delete next;
+            next=NULL;
+        }
+        cout<<"MEMORY IS FREE FOR NODE WITH DATA "<<value<<endl;
+    }
+}
+int main(){
+    return 0;
+}
