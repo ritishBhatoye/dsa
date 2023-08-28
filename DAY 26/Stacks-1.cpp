fdc3b1e8dfb812217be7f153c6f55118bfafcd36
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-28 23:08:21 +0530

    DSA

---
+++ DAY 26/Stacks-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+    //PROPERTIES
+    public:
+      int *arr;
+      int top;
+      int size;
+
+
+//behaviour
+
+Stack(int size){
+
+}
+};
+
+int main(){
+    return 0;
+}
