ea07e63aaf80be202d5c06c52c6a4e6eeadb106f
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-28 23:58:46 +0530

    DSA

--- DAY 26/Stacks-1.cpp
+++ DAY 26/Stacks-1.cpp
@@ -17,10 +17,48 @@ arr=new int[size];
 top=-1;
 }
 void push(int element){
-    
+if(size-top > 1){
+top++;
+arr[top]=element;
+}
+else{
+cout<<"Stack OverFlow "<<endl;
+}
+}
+void pop(){
+if(top>=0){
+top--;
+}
+else{
+cout<<"Stack UnderFlow "<<endl;
+}
+}
+int peek(){
+if(top>=0){
+    return arr[top];
+}
+else{
+    cout<<"Stack is Empty "<<endl;
+    return -1;
+}
+}
+bool isEmpty(){
+if(top==-1){
+    return true;
+}
+else{
+    return false;
+}
 }
 };
 
 int main(){
+
+    Stack st(5);
+    st.push(20);
+    st.push(42);
+    st.push(44);
+    
+    cout<<st.peek()<<endl;
     return 0;
 }
