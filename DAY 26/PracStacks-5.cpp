02378a086fd3e9d77804f18c934d4e9bcf2061fc
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-29 12:29:24 +0530

    DSA

---
+++ DAY 26/PracStacks-5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+public:
+int size;
+int *arr;
+int top;
+
+Stack(int size){
+    this->size=size;
+    top=-1;
+}
+void push(int element){
+    if(size - top > 1)
+    {
+    
+    }
+    else
+    {
+    
+    }
+}
+};
+
+
+int main(){
+    return 0;
+}
