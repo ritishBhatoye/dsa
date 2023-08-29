c809e7583ecf35892b69c9f9613a51057e8e634c
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-29 13:21:20 +0530

    DSA

--- DAY 26(STACKS)/DeleteElementFromStack.cpp
+++ DAY 26(STACKS)/DeleteElementFromStack.cpp
@@ -1,7 +1,61 @@
 #include<iostream>
-#include<stack>
+// #include<stack>
 using namespace std;
 
+class Stack{
+    public:
+    int size;
+    int *arr;
+    int top;
+
+    Stack(int size){
+     this->size=size;
+     top=-1;
+    }
+
+    void push(int element){
+        if(size - top > 1){
+            top++;
+            arr[top]=element;
+
+        }
+        else{
+            cout<<"STACK OVERFLOW "<<endl;
+        }
+    }
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        else{
+            cout<<"STACK UNDERFLOW "<<endl;
+        }
+    }
+    
+    int peek(){
+        if(top>=0){
+            return arr[top];
+        }
+        else{
+            return -1;
+        }
+    }
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
 int main(){
+    Stack s(40);
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout<<s.peek()<<endl;
+    
     return 0;
 }
