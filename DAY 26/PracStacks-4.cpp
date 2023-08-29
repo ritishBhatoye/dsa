499cadf7de04c5caa110fc416e52f26b2c2de793
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-29 12:13:36 +0530

    DSA

--- DAY 26/PracStacks-4.cpp
+++ DAY 26/PracStacks-4.cpp
@@ -1,49 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Stack{
-    public:
+class Stack
+{
+public:
     int *arr;
     int top1;
     int top2;
     int size;
 
-    Stack(int size){
-        this->size=size;
-        top1=-1;
-        top2=size;
+    Stack(int size)
+    {
+        this->size = size;
+        top1 = -1;
+        top2 = size;
     }
 
-    void push1(int element){
-        if(size-top1 > 1){
+    void push1(int element)
+    {
+        if (size - top1 > 1)
+        {
             top1++;
-            arr[top1]=element;
+            arr[top1] = element;
         }
-        else{
-            cout<<"STACK OVERFLOW "<<endl;
+        else
+        {
+            cout << "STACK OVERFLOW " << endl;
         }
     }
-    void push2(int element){
-        if(top2-top1){
+    void push2(int element)
+    {
+        if (top2 - top1)
+        {
             top2--;
-            arr[top2]=element;
+            arr[top2] = element;
         }
-        else{
-            cout<<"STACK UNDERFLOW "<<endl;
+        else
+        {
+            cout << "STACK UNDERFLOW " << endl;
         }
     }
-    void pop1(){
-    if(top1>=0){
-        top1--;
-    }
-    else{
-        cout<<"STACK UNDERFLOW "<<endl;
-    }
+    int pop1()
+    {
+        if (top1 >= 0)
+        {
+            int ans = arr[top];
+            top1--;
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
     }
 
-    void pop2(){
-        if(top2)
+    int pop2()
+    {
+        if (top2 < size)
+        {
+            int ans = arr[top2];
+            top2++;
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
     }
 };
-int main(){
+int main()
+{
+
     return 0;
 }
