f6fe4f745c3ee3adbde978c12c5fcce65b71634d
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-29 07:46:02 +0530

    DSA

---
+++ DAY 26/PracStacks-1.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+class Stack{
+    public:
+    int top;
+    int *arr;
+    int size;
+
+    Stack(int size){
+        this->size=size;
+        arr=new int[size];
+        top=-1;
+    }
+    void push(int element){
+        if(size-top > 1){
+            top++;
+            arr[top]=element;
+        }
+        else{
+            cout<<"STACK OVERFLOW "<<endl;
+        }
+    }
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        else{
+            cout<<"STACK UNDERFLOW "<<endl;
+        }
+    }
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int peek(){
+       if(top>=0){ 
+        return arr[top];
+        }
+       else{
+        cout<<"Stack is EMPTY "<<endl;
+        return -1;
+       }
+    }
+};
+int main(){
+    Stack st(5);
+    st.push(10);
+    st.push(15);
+    st.push(20);
+    st.push(30);
+
+    st.peek();
+    return 0;
+}
