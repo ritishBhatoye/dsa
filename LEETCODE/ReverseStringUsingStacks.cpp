4b0366fdb7d3ab46cb970af3e8164664127fa5d8
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-29 12:42:36 +0530

    DSA

--- LEETCODE/ReverseStringUsingStacks.cpp
+++ LEETCODE/ReverseStringUsingStacks.cpp
@@ -4,6 +4,20 @@ using namespace std;
 int main(){
 
     string s="RITISH";
+
+    stack<char> a;
+
+    for(int i=0;i<s.length();i++){
+     char ch=s[i];
+     a.push(ch);
+    }
+
+    string ans="";
+    while(!a.empty()){
     
+    ans.push_back();
+    a.pop();
+
+    }
     return 0;
 }
