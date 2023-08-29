8aa33da7307aedef1c17bcd33aa70212a6dd2161
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-29 12:37:14 +0530

    DSA

--- DAY 26/PracStacks-5.cpp
+++ DAY 26/PracStacks-5.cpp
@@ -1,29 +1,25 @@
 #include<iostream>
 using namespace std;
-
-class Stack{
-public:
-int size;
-int *arr;
-int top;
-
-Stack(int size){
-    this->size=size;
-    top=-1;
-}
-void push(int element){
-    if(size - top > 1)
-    {
-    
-    }
-    else
-    {
-    
-    }
-}
-};
-
-
+#include<stack>
 int main(){
+    string s="RITISH";
+
+    stack<char> a;
+
+    for(int i=0;i<s.length();i++){
+        char ch=s[i];
+        a.push(ch);
+    }
+
+    string ans="";
+
+    while(!a.empty()){
+        char ch=a.top();
+        ans.push_back(ch);
+
+        a.pop();
+    }
+
+    cout<<ans<<endl;
     return 0;
 }
