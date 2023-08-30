31dc14680d025ac3f9cbb9a619f39863e1b8f2bd
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-30 10:21:11 +0530

    DSA

--- DAY 26(STACKS)/InsertingElementAtBottom.cpp
+++ DAY 26(STACKS)/InsertingElementAtBottom.cpp
@@ -1,7 +1,18 @@
 //INSERTING ELEMENT AT BOTTOM 
 #include<iostream>
+#include<stack>
 using namespace std;
 
+void pushAtBottom(stack<int> s,int x){
+    solve(s,x);
+}
 int main(){
+    stack<int> s;
+    s.push(3);
+    s.push(5);
+    s.push(7);
+    s.push(9);
+    cout<<s.top();
+
     return 0;
 }
