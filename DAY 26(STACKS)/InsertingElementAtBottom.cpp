c323efda69f9cb4802095a3e318f3fe64dc4c0c7
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-30 10:38:12 +0530

    DSA

--- DAY 26(STACKS)/InsertingElementAtBottom.cpp
+++ DAY 26(STACKS)/InsertingElementAtBottom.cpp
@@ -2,9 +2,33 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-
-void pushAtBottom(stack<int> s,int x){
+void solve(stack<int> &s,int x){
+    if(s.empty()){
+        s.push(x);
+        return ;
+    }
+    int num=s.top();
+     s.pop();
+    //RECURSSIVE CALL
     solve(s,x);
+    s.push(num);
+
+}
+void pushAtBottom(stack<int> &s,int x){
+    solve(s,x);
+}
+int input(){
+    int x;
+    cout<<"ENTER THE ELEMENT YOU WOULD LIKE TO INSERT "<<endl;
+    cin>>x;
+    return x;
+}
+
+void print(stack<int> &s){
+while(!s.empty()){
+    cout<<s.top()<<endl;
+    s.pop();
+}
 }
 int main(){
     stack<int> s;
@@ -12,7 +36,10 @@ int main(){
     s.push(5);
     s.push(7);
     s.push(9);
-    cout<<s.top();
+    int x=input();
+    pushAtBottom(s,x);
+    cout<<"PUSHING ELEMENT AT BOTTOM "<<endl;
+    print(s);
 
     return 0;
 }
