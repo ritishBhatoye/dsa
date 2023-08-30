4a048a8e907a228f1c86dfc08507ab1d0be7bbdf
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-30 17:23:42 +0530

    DSA

--- DAY 27(ARRAYS)/PracArrays.cpp
+++ DAY 27(ARRAYS)/PracArrays.cpp
@@ -15,13 +15,14 @@ for(int i=0;i<size;i++){
 }
 int sum(int *arr,int *arr1,int size,int size1){
     int result=0;
-    for(int i=0;i<arr;i++){
+    for(int i=0;i<size;i++){
         result+=arr[i];
     }
-    for(int i=0;i<arr1;i++)
-{
+    for(int i=0;i<size1;i++)
+   {
     result+=arr1[i];
-}
+   }
+   return result;
 }
 
 int main(){
@@ -37,5 +38,8 @@ cin>>size1;
 int arr1[size1];
 input(arr1,size1);
 print(arr1,size1);
+
+cout<<"SUM OF TWO ARRAY IS "<<endl;
+cout<<sum(arr,arr1,size,size1)<<endl;
     return 0;
 }
