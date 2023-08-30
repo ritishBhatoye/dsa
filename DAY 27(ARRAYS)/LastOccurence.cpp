024873f1fc2c790c01828eab16306ecacbecf0b5
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-30 19:35:40 +0530

    DSA

--- DAY 27(ARRAYS)/LastOccurence.cpp
+++ DAY 27(ARRAYS)/LastOccurence.cpp
@@ -1,44 +1,27 @@
 #include<iostream>
 using namespace std;
 
-void input(int *arr,int size){
-    cout<<"ENTER THE ELEMENT YOU WANT TO INSERT "<<endl;
+void input(){}
+void print(){}
+int isPresent(int *arr,int size,int e){
+    
+    int ans;
     for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-}
-void print(int *arr,int size){
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<endl;
-    }
-}
-int Min(int *arr,int size){
-    int ans=INT_MAX;
-for(int i=0;i<size;i++){
-    if(arr[i]<ans){
-       ans=arr[i];
-    }
-  }
-  return ans;
-}
-int Max(int *arr,int size){
-    int ans=INT_MIN;
-    for(int i=0;i<size;i++){
-        if(arr[i]>ans){
-            ans=arr[i];
-        }
+       if(e==arr[i])
+       {
+         ans=i;
+         break;
+       }
+       else{
+        ans=-1;
+       }
     }
     return ans;
 }
 int main(){
-    int *arr;
-    int size;
-    cout<<"ENTERT THE SIZE OF AN ARRAY "<<endl;
-    cin>>size;
-    input(arr,size);
-    print(arr,size);
-    cout<<"MINIMUM ELEMENT IN THE ARRAY IS "<<Min(arr,size)<<endl;
-    cout<<"MAXMIMUM ELEMENT IN THE ARRAY IS "<<Max(arr,size)<<endl;
+   int arr[8]={7, 5, 2, 11, 2, 43, 1, 1};
+   cout<<isPresent(arr,8,10)<<endl;
+   
 
     return 0;
 }
