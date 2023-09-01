cdff85d415b632d6857afc72d3cfa454f296bae7
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-01 08:43:30 +0530

    DSA

--- DoublyEndedQueue.cpp
+++ DoublyEndedQueue.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main(){
     deque<int> d;
     d.push_front(12);
-    
+    d.push_back(14);
+    cout<<d.front()<<endl;
+    cout<<d.back()<<endl;
+
+    d.pop_front();
+    cout<<d.front()<<endl
+    cout<<d.back()<<endl;
     return 0;
 }
