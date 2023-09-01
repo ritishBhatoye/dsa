9d0d98d21e533ab6a0fe93261e7f1f2880e6dfa6
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-01 08:44:42 +0530

    DSA

--- DoublyEndedQueue.cpp
+++ DoublyEndedQueue.cpp
@@ -10,7 +10,14 @@ int main(){
     cout<<d.back()<<endl;
 
     d.pop_front();
-    cout<<d.front()<<endl
+    cout<<d.front()<<endl;
     cout<<d.back()<<endl;
+    d.pop_back();
+    if(d.empty()){
+        cout<<"QUEUE IS EMPTY "<<endl;
+    }
+    else{
+        cout<<"QUEUE IS NOT EMPTY "<<endl;
+    }
     return 0;
 }
