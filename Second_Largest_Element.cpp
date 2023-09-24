3db6ce9fbba1c8cd36b2bc03957e00835a1794c2
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-24 10:01:59 +0530

    DSA

---
+++ Second_Largest_Element.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void input(int *arr,int size){
+for(int i=0;i<size;i++){
+    cin>>arr[i];
+}
+}
+int SecondLargestElement(int *arr,int size){
+int ans=0;
+int i=0;
+
+}
+
+int main(){
+    return 0;
+}
