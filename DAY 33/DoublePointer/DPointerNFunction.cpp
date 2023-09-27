b149c033b83fa0af37849d52241ad500581881f1
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-27 20:27:12 +0530

    DSA

--- DAY 33/DoublePointer/DPointerNFunction.cpp
+++ DAY 33/DoublePointer/DPointerNFunction.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+void print(int **q){
+    cout<<**q<<endl;
+}
+void update(int **q){
+    **q=**q+1;
+}
 int main(){
+    int a=10;
+    int *p=&a;
+    int **q=&p;
+    cout<<"BEFORE "<<endl;
+    print(q);
+    update(q);
+    cout<<"AFTER  "<<endl;
+    print(q);
+
     return 0;
 }
