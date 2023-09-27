a10a8cbc85e08d0f13263d4380f55574c76beac3
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-27 06:32:50 +0530

    DSA

---
+++ DAY 33/pointers/Intro1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=10;
+    int *a=&num;
+    cout<<"ADDRESS OF NUM    : "<<a<<endl;
+    cout<<"VALUE OF NUM      : "<<*a<<endl;
+    cout<<"ADDRESS OF A(ptr) : "<<&a<<endl; 
+    cout<<"SIZE OF OPERATOR  : "<<sizeof(a)<<endl;
+    cout<<"SIZE OF POINTER   : "<<sizeof(&num)<<endl;
+    cout<<"SIZEN OF POINTER  : "<<sizeof(a)<<endl; 
+    return 0;
+}
