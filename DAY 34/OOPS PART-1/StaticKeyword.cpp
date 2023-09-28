d2ca6415600244ea6d3b9608eaf93f1d9c7a9890
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 21:39:43 +0530

    DSA

--- DAY 34/OOPS PART-1/StaticKeyword.cpp
+++ DAY 34/OOPS PART-1/StaticKeyword.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
 using namespace std;
+class Student{
+public:
+int age;
+static int about;
+static int random(){
+   return about;
+}
+};
+int Student::about=10;
 int main(){
+    cout<<endl<<endl;
+cout<<Student::about<<endl; 
+    cout<<endl<<endl;
+    cout<<Student::random()<<endl;
+    cout<<endl<<endl;
+    
+
 return 0;
 }
