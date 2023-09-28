6d24380af3dc1d03c1fc000ad583de1749fce852
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 22:30:06 +0530

    DSA

--- DAY 34/OOPS PART-2/ENCAPSULATION.cpp
+++ DAY 34/OOPS PART-2/ENCAPSULATION.cpp
@@ -5,8 +5,19 @@ private:
 string name;
 int age;
 int height;
+
+public:
+int getAge(){
+    return this->age;
+}
 };
 int main(){
+    Student s;
+ 
+    cout<<endl<<endl;
+
+    cout<<"AGE : "<<s.getAge()<<endl;
+    cout<<endl<<endl;
     
     return 0;
 }
