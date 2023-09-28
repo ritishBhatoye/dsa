6a15e24ddb985fd1595df28df51458d6c6007cab
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 10:06:50 +0530

    DSA

--- DAY 34/OOPS PART-1/Intro_1.cpp
+++ DAY 34/OOPS PART-1/Intro_1.cpp
@@ -1,5 +1,35 @@
 #include<iostream>
 using namespace std;
+
+class Student{
+    public:
+    int rollNo;
+    string name;
+    string Class;
+    int age;
+    void pName(){
+        cout<<"STUDENT NAME     : "<<name<<endl;
+    }
+    void pRollNo(){
+        cout<<"STUDENT ROLL NO. : "<<rollNo<<endl;
+    }
+    void pClass(){
+        cout<<"STUNDET CLASS    : "<<Class<<endl;
+    }
+    void pAge(){
+        cout<<"STUDENT AGE      : "<<age<<endl;
+    }
+
+};
 int main(){
+    Student st;
+    st.name="RITISH";
+    st.rollNo=27;
+    st.Class="XII";
+    st.age=18;
+    st.pName();
+    st.pRollNo();
+    st.pClass();
+    st.pAge();
     return 0;
 }
