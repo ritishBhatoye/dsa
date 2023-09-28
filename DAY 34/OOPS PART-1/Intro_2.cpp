d803d7cc0f3d34b709d2697d84b27a66e60ae197
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 11:54:28 +0530

    DSA

--- DAY 34/OOPS PART-1/Intro_2.cpp
+++ DAY 34/OOPS PART-1/Intro_2.cpp
@@ -2,12 +2,37 @@
 using namespace std;
 //GETTER AND SETTERS
 class Hero{
+    //PROPERTIES
+    private:
+    int health;
 public:
+char level;
 
+void setHealth(int h){
+health=h;
+}
+int getHealth(){
+    return health;
+}
+void setLevel(char l){
+    level=l;
+}
+char getLevel(){
+    return level;
+}
 };
 
 int main(){
     Hero h;
-    
+    cout<<endl<<endl<<endl;
+    //setting values
+    h.setHealth(100);
+    h.setLevel('H');
+    cout<<"LEVEL  : "<<h.getLevel()<<endl;
+    cout<<"HEALTH : "<<h.getHealth()<<endl;
+    cout<<endl;
+    cout<<"SIZE OF HERO "<<endl;
+    cout<<sizeof(h)<<endl;
+    cout<<endl<<endl<<endl;
     return 0;
 }
