ca176085adec2e1e0b0d477e9976e27279b7b11f
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 16:33:32 +0530

    DSA

--- DAY 34/OOPS PART-1/ShallowNDeepCopy/ShallowNDeepCopy.cpp
+++ DAY 34/OOPS PART-1/ShallowNDeepCopy/ShallowNDeepCopy.cpp
@@ -8,7 +8,20 @@ Student(){
     cout<<"DEFAULT CONSTRUCTOR IS CALLED "<<endl;
     name=new char[100];
 }
+void setName(char name[]){
+    strcpy(this->name,name);
+}
+void print(){
+    cout<<"NAME : "<<this->name<<endl;
+}
 };
+
 int main(){
+    Student s;
+    cout<<endl<<endl;
+    char name[7]="RITISH";
+    s.setName(name);
+    s.print();
+    cout<<endl<<endl;
     return 0;
 }
