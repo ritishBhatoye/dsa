97bb2a4c37d5bcaad733f003fa565d1b283ad227
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 09:59:37 +0530

    DSA

--- DAY 34/OOPS PART-1/INTRO.cpp
+++ DAY 34/OOPS PART-1/INTRO.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
+//class
+class Hero{
+    public:
+    //properties or MEMBER FUNCTION
+    // char name[100];
+    // int health;
+    char level;
+    int health;
+};
 int main(){
-    
+Hero h;
+h.health=10;
+h.level='H';
+cout<<endl;
+cout<<endl;
+cout<<"HEALTH : "<<h.health<<endl;
+cout<<"LEVEL  : "<<h.level<<endl;
+cout<<endl;
     return 0;
 }
