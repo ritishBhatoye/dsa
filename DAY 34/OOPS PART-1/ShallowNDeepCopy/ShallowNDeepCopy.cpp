aa6459f80ae2d7e601c569a0e8e3d293df147709
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 16:28:16 +0530

    DSA

--- DAY 34/OOPS PART-1/ShallowNDeepCopy/ShallowNDeepCopy.cpp
+++ DAY 34/OOPS PART-1/ShallowNDeepCopy/ShallowNDeepCopy.cpp
@@ -3,7 +3,11 @@ using namespace std;
 class Student{
 public:
 int age;
-string name;
+char *name; 
+Student(){
+    cout<<"DEFAULT CONSTRUCTOR IS CALLED "<<endl;
+    name=new char[100];
+}
 };
 int main(){
     return 0;
