0e849783cd6bd45f09ce37f936a5e7babb0a8ac7
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-28 15:59:53 +0530

    DSA

--- DAY 34/OOPS PART-1/Copy_Constructor.cpp
+++ DAY 34/OOPS PART-1/Copy_Constructor.cpp
@@ -3,15 +3,24 @@ using namespace std;
 class Student{
     public:
     string name;
-    // Student(string name){
-    //     this->name=name;
-    // }
-    Student(){
-        cout<<"DEFAULT CONSTRUCTOR CALLED "<<endl;
+    int age;
+    Student(string name,int age){
+        this->name=name;
+        this->age=age;
     }
 };
 int main(){
-    Student St;
-    
+    Student St("RAHUL",18);
+    //COPYING St OBJECT'S DATA VALUES TO S OBJECT
+    Student S(St);
+    cout<<endl<<endl;
+    cout<<"ST NAME "<<St.name<<endl;
+    cout<<"ST AGE  "<<St.age<<endl;
+    cout<<endl<<endl;
+    cout<<"S NAME "<<S.name<<endl;
+    cout<<"S AGE  "<<S.age<<endl;
+
+    cout<<endl<<endl;
+
     return 0;
 }
