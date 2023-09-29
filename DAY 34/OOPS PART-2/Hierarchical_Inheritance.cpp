7ab92386389691e7171543eb4bbe40699def9ae9
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-29 08:45:48 +0530

    DSA

--- DAY 34/OOPS PART-2/Hierarchical_Inheritance.cpp
+++ DAY 34/OOPS PART-2/Hierarchical_Inheritance.cpp
@@ -3,9 +3,33 @@ using namespace std;
 
 class A{
     public:
-    
+    void fun1(){
+        cout<<"INSIDE A "<<endl;
+    }
+};
+class B:public A{
+    public:
+    void fun2(){
+        cout<<"INSIDE B "<<endl;
+    } 
+};
+
+class C:public A{
+public:
+void fun3(){
+    cout<<"INSIDE C "<<endl;
 }
+};
 
 int main(){
+  
+  C c;
+  cout<<endl<<endl;
+  c.fun1();
+  c.fun3();
+   B b;
+   b.fun1();
+   b.fun2();
+  cout<<endl<<endl;
     return 0;
 }
