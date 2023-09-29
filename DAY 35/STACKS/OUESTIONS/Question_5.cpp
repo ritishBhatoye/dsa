6db2319ddb51f2dfc22c9e92944a65a4c1232c37
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-29 21:46:39 +0530

    DSA

--- DAY 35/STACKS/OUESTIONS/Question_5.cpp
+++ DAY 35/STACKS/OUESTIONS/Question_5.cpp
@@ -2,7 +2,34 @@
 #include<iostream>
 #include<stack>
 using namespace std;
+
+void InsertEleAtBottom(stack<int> &s,int element){
+if(s.empty()){
+ s.push(element); 
+ return ;  
+}
+int num=s.top();
+s.pop();
+//RECURSIVE CALL
+InsertEleAtBottom(s,element);
+s.push(num);
+}
+
 int main(){
-    
+stack<int> s;
+s.push(10);
+s.push(20);
+cout<<endl<<endl;
+int element;
+cout<<"ENTER THE ELEMENT TO INSERT IN A STACK "<<endl;
+cin>>element;
+InsertEleAtBottom(s,element);
+do{
+    cout<<s.top()<<" ";
+    s.pop(); 
+}
+while(!s.empty());
+cout<<endl<<endl;
+
     return 0;
 }
