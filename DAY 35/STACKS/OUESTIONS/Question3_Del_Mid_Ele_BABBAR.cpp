2fcbb2f87f3d50b4e99bc828337a83ac1aa8b1d9
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-29 20:13:42 +0530

    DSA

--- DAY 35/STACKS/OUESTIONS/Question3_Del_Mid_Ele_BABBAR.cpp
+++ DAY 35/STACKS/OUESTIONS/Question3_Del_Mid_Ele_BABBAR.cpp
@@ -1,6 +1,28 @@
 //DELELTING MIDDLE ELEMENT FROM AN ARRAY 
 #include<iostream>
 using namespace std;
+class Stack{
+    public:
+    int size;
+    int *arr;
+    int top;
+    Stack(int size){
+        this->size=size;
+        top=-1;
+        arr=new int[size];
+    }
+    void push(int num){
+        if(size-top>1){
+            top++;
+            arr[top]=num;
+        }
+        else{
+            cout<<"STACK OVERFLOW "<<endl;
+        }
+    }
+    void 
+};
 int main(){
+
     return 0;
 }
