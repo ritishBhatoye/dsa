ed2923e43b156df4b311e35254024e1f747eee53
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-09-30 14:01:54 +0530

    DSA

---
+++ DAY 36/StackPractice/SortStack.cpp
@@ -0,0 +1,49 @@
+//SORTING STACK
+//APPROACH :-
+//
+#include<iostream>
+#include<stack>
+using namespace std;
+void insertAtBottom(stack<int> &s,int num){
+    //Base Case
+    if((s.top()<num && !s.empty())|| s.empty()){
+        s.push(num);
+        return;
+    }
+    int x=s.top();
+    s.pop();
+
+    //recursive call
+    insertAtBottom(s,num);
+
+    s.push(x);
+}
+void sortStack(stack<int> &s){
+    //base case
+    if(s.empty()){
+        return;
+    }
+    int num=s.top();
+    s.pop();
+    //recursive call
+    sortStack(s);
+
+    insertAtBottom(s,num);
+}
+int main(){ 
+stack<int> s;
+s.push(4);
+s.push(80);
+s.push(60);
+s.push(50);
+s.push(10);
+cout<<endl<<endl;
+sortStack(s);
+do{
+    cout<<s.top()<<" ";
+    s.pop();
+   }
+   while(!s.empty());
+   cout<<endl<<endl;
+    return 0;
+}
