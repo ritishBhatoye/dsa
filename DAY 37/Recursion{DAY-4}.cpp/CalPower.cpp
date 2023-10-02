c2d53a5d6672dd014cc9556e80caec259bf30bfe
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-02 15:28:17 +0530

    DSA

--- DAY 37/Recursion{DAY-4}.cpp/CalPower.cpp
+++ DAY 37/Recursion{DAY-4}.cpp/CalPower.cpp
@@ -10,6 +10,27 @@ int Power(int a,int b){
 
     return a*Power(a,b-1);
 }
+//LOVE BABBAR SIR JI APPROACH
+int powOptimalAp(int a,int b){
+
+    //base case
+    if(b==0){
+        return 1;
+    }
+    if(b==1){
+        return a;
+    }
+    //recursive call
+    int ans=powOptimalAp(a,b/2);
+    if(b%2==0){
+        //if b  is even
+        return ans*ans;
+    }
+    else{
+        //if b is odd
+        return a*ans;
+    }
+}
 
 int main(){
     int a,b;
