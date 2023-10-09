a00f4663922c0c8f6d141d3b045da149d13667f2
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-09 21:37:48 +0530

    DSA

---
+++ DAY 43/StackQUESTION/DeleteElementFromMiddleOfStack.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void DeleteElementFromMiddleOfStack(stack &s){
+ if(s.top()==(s.size()/2)){
+    
+ }
+}
+int main(){
+    stack<int> s;
+    s.push(10);
+    cout<<"TOP ELEMENT : "<<s.top()<<endl;
+    cout<<"SIZE OF STACK : "<<s.size()<<endl;
+    return 0;
+}
---
+++ DAY 43/StackQUESTION/intro.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<endl<<endl;
+    cout<<"HI "<<endl;
+    cout<<endl<<endl;
+
+    return 0;
+}
