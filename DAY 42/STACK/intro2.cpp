ef5ee7f17be7140bea0425d223fe2b52f9da368c
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-09 14:52:41 +0530

    DSA

--- DAY 42/STACK/intro2.cpp
+++ DAY 42/STACK/intro2.cpp
@@ -1,63 +1,66 @@
 #include<iostream>
 using namespace std;
 
-class Stack
-{
+class Stack{
     public:
-     int size;
-     int *arr;
-     int top;
-     Stack(int size){
+    int *arr;
+    int size;
+    int top;
+
+    Stack(int size)
+    {
         this->size=size;
         arr=new int[size];
         top=-1;
-     }
-    
-     void push(int element){
-        if(size-top>1){
+    }
+    //PUSH OPERATION 
+    void push(int element){
+       if(size-top>1){ 
         top++;
         arr[top]=element;
         }
         else{
-            cout<<"STACK OVERFLOW "<<endl;
+            cout<<"STACK OVERFLOW"<<endl;
         }
-     }
-     void pop(){
-        if(top>=0){
-            top--;
+    }
+    void pop(){
+        if(top>=0)
+        {
+        top--;
         }
-        else{
-            cout<<"STACK UNDERFLOW "<<endl;
+        else
+        {
+        cout<<"STACK UNDERFLOW "<<endl;
         }
-     }
-     int peak(){
+    }
+    
+    int peak(){
         if(top>=0){
             return arr[top];
-        } 
+        }
         else{
             cout<<"STACK IS EMPTY "<<endl;
             return -1;
         }
-     }
-     bool isEmpty(){
-          if(top==-1)
-          {
-              return true;
-          }
-          else 
-          {
-            return false;
-          }
-     }
+    }
+   bool isEmpty(){
+    if(top==-1){
+return true;
+    }
+    else{
+return false;
+    }
+   }
 };
 
-int main(){
-    Stack s(5);
+int main()
+{   Stack s(5);
     s.push(10);
     s.push(20);
     s.push(30);
+    s.push(40);
     cout<<endl<<endl;
-    cout<<s.peak()<<endl;
+    cout<<"TOP ELEMENT "<<s.peak()<<endl;
     if(s.isEmpty()){
         cout<<"STACK IS EMPTY "<<endl;
     }
