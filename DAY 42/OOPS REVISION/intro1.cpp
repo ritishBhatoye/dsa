e57143f73029b88ec77f3d356c71903541a9726e
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-09 13:20:23 +0530

    DSA

---
+++ DAY 42/OOPS REVISION/intro1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Human{
+public:
+int weight;
+int height;
+};
+
+int main(){
+Human h;
+h.weight=100;
+h.height=6;
+cout<<endl<<endl;
+cout<<h.weight<<endl;
+cout<<h.height<<endl;
+cout<<endl<<endl;
+
+
+    return 0;
+}
