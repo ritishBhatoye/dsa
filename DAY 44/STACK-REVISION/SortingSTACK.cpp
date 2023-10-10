e746b731c23bd637c5d2b1cd8da36d776ecf3969
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-10 22:42:38 +0530

    DSA

---
+++ DAY 44/STACK-REVISION/SortingSTACK.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void SortStack(stack<int> &s){
+    if(s.empty()){
+        return ;
+
+    }
+    if()
+}
+
+int main()
+{
+    return 0;
+}
