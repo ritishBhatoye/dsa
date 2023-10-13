35a6ece519a3cf3438c4030e78ad6e3f074c20a2
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-14 02:38:43 +0530

    DSA

---
+++ DAY 50/Questions/QueueReversal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void QueueReversal(queue<int> q){
+    if(q.empty()){
+        return ;
+    }
+    
+}
+
+int main(){
+    return 0;
+}
