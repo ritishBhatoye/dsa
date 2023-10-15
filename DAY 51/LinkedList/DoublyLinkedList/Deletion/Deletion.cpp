380fe60dfdf464c08770a077b0ba5e7da62cc611
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-16 03:11:12 +0530

    DSA

--- DAY 51/LinkedList/DoublyLinkedList/Deletion/Deletion.cpp
+++ DAY 51/LinkedList/DoublyLinkedList/Deletion/Deletion.cpp
@@ -16,6 +16,12 @@ public:
         this->next=NULL;
         this->prev=NULL;
       }
+      ~Node(){
+        if(this->next!=NULL){
+            delete next;
+            next=NULL;
+        }
+      }
 };
 
 //print or traversing
@@ -73,7 +79,52 @@ temp->next->prev=newNode;
 temp->next=newNode;
 newNode->prev=temp;
 }
+//deletion of node
+void deleteNode(Node* &head,Node* &tail,int position){
+    //deletion of FIRST NODE
+    Node* temp=head;
 
+    if(position==1)
+    {
+     temp->next->prev=NULL;
+     head=temp->next;
+     temp->next=NULL;
+
+     //memory free
+     delete temp;
+    }
+
+    else
+    {
+        Node* c=temp;
+        Node* p=NULL;
+        int cnt=1;
+    
+      while(cnt<position){
+        p=c;
+        c=c->next;
+        cnt++;
+      }
+      //LAST NODE
+      if(c->next==NULL)
+      {     
+            c->prev=NULL;
+            p->next=NULL;
+            tail=p;
+            delete c;
+      }
+
+        //in between
+    else
+    {
+        p->next=c->next;
+        c->next->prev=p;
+        c->next=NULL;
+        c->prev=NULL;
+        delete c;
+      }
+    }
+}
 int main()
 {
     Node* n=new Node(10);
@@ -93,6 +144,21 @@ int main()
     print(head);
     insertAtPosition(head,tail,1,-1);
     print(head);
+    cout<<"NODE AT 1st POSITION : "<<endl;
+    deleteNode(head,tail,1);
+    print(head);
+    cout<<"NODE AT 3rd POSITION : "<<endl;
+    deleteNode(head,tail,3);
+    print(head);
+    cout<<"DELETING LAST NODE : "<<endl;
+    deleteNode(head,tail,5);
+    print(head);
+    cout<<"TAIL DATA : "<<tail->data<<endl;
+    cout<<"TAIL NEXT : "<<tail->next<<endl;
+    cout<<"HEAD DATA : "<<head->data<<endl;
+    cout<<"HEAD NEXT : "<<head->next<<endl;
+
+
     cout<<endl<<endl;
 
     return 0;
