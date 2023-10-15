44bc8bc51559d312e1199b8ee627f4fd6e0cc6ff
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-10-15 10:24:29 +0530

    DSA

---
+++ DAY 50/Questions/KQueues.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+using namespace std;
+
+class kQueue
+{
+   public:
+   int n;
+   int k;
+   int *front;
+   int *rear;
+   int *arr;
+   int freespot;
+   int *next;
+
+   kQueue(int n,int k)
+   {
+    this->n=n;
+    this->k=k;
+    front=new int[k];
+    rear=new int[k];
+    
+    for(int i=0;i<k;i++){
+         front[i]=-1;
+         rear[i]=-1;
+    }
+    next=new int[n];
+    for(int i=0;i<n;i++){
+        next[i]=i+1;
+    }
+    next[n-1]=-1;
+    arr=new int[n];
+    freespot=0;
+    
+   }
+
+   void enqueue(int data,int qn){
+    if(freespot==-1){
+        cout<<"NOT EMPTY SPACE IS AVAILABLE "<<endl;
+        return ;
+    }
+
+    //find first free index
+    int index=freespot;
+
+    //update freespot
+    freespot=next[index];
+
+    //check whether first element
+
+   if(front[qn-1]==-1){
+        //if FIRST ELEMENT
+        front[qn-1]=index;
+
+    }
+    else{
+        //link new element to the previous element 
+        next[rear[qn-1]]=index;
+    }
+    //update next
+    next[index]=-1;
+    //update rear
+    rear[qn-1]=index;
+
+    //push element
+    arr[index]=data;
+
+   }
+
+//DEQUEUE
+int dequeue(int qn){
+    //underflow
+    if(front[qn-1]==-1){
+        cout<<"NOT A SINGLE ELEMENT IS PRESENT "<<endl;
+        return -1;
+    }
+    
+    //find indext to pop
+    int index=front[qn-1];
+    
+    //front ko aage bdao
+    front[qn-1]=next[index];
+    //freeslot ko manage kro
+    next[index]=freespot;
+
+    freespot=index;
+    return arr[index];
+
+}
+
+};
+
+int main(){
+    kQueue q(10,3);
+    q.enqueue(10,1);
+    q.enqueue(15,1);
+    q.enqueue(20,2);
+    q.enqueue(25,1);
+
+    cout<<endl<<endl;
+    cout<<q.dequeue(1)<<endl;
+    cout<<q.dequeue(2)<<endl;
+    cout<<q.dequeue(1)<<endl;
+    cout<<q.dequeue(1)<<endl;
+
+
+
+    cout<<endl<<endl;
+
+
+    
+    return 0;
+}
