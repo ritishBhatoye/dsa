ec92d133fe47b32e444a3990a1c9bb083a22445e
Ritish Bhatoye <ritishrahul87@gmail.com>
2024-03-05 10:00:29 +0530

    DSA

--- DAY 84/MaxHeap.cpp
+++ DAY 84/MaxHeap.cpp
@@ -1,50 +1,50 @@
 #include<iostream>
 using namespace std;
 
+class MaxHeap{
 
-class heap{
-    public:
-    int arr[100];
-    int size=0;
-    heap(){
-        arr[0]=-1;
-        size=0;
-    }
+   public:
+   int arr[100];
+   int size;
 
-    void insert(int val){
-        size=size+1;
-        int index=size;
-        arr[index]=val;
+   MaxHeap(){
+    arr[0]=-1;
+    size=0;
+   }
 
-        while(index > 1){
-            int parent=index/2;
-            if(arr[parent] < arr[index]){
-                swap(arr[parent],arr[index]);
-                index=parent;
-            }
-            else{
-                return ;
-            }
+   void insert(int val){
+    size=size+1;
+    int index=size;
+    arr[index]=val;
+    while(index > 1){
+        int parent=index/2;
+        if(arr[parent]<arr[index]){
+           swap(arr[parent],arr[index]);
+           index=parent;
+        }
+        else{
+            return ;
+
         }
     }
+   }
 
-    void print(){
-        for(int i=1;i<=size;i++){
-            cout<<arr[i]<<" ";
-        }
-        cout<<endl;
-    }
+   void print(){
+    for(int i=1;i<=size;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+   }
 };
-
 
 int main(){
 
-heap h;
-h.insert(50);
-h.insert(55);
-h.insert(53);
-h.insert(52);
-h.insert(54);
-h.print();
+    MaxHeap m;
+    m.insert(10);
+    m.insert(20);
+    m.insert(30);
+    m.insert(40);
+    m.insert(50);
+    m.print();
+
     return 0;
 }
---
+++ DAY 84/MinHeap.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+class MinHeap{
+    public:
+    int arr[100];
+    int size;
+
+    MinHeap(){
+    arr[0]=-1;
+    size=0;
+    }
+
+    void insert(int val)
+    {
+        size=size+1;
+        int index=size;
+        arr[index]=val;
+
+        while(index > 1 ){
+            int parent =index/2;
+
+            if(arr[parent] > arr[index])
+            {
+                swap(arr[parent],arr[index]);
+                index=parent;
+            }
+            else
+            {
+                 return ;
+            }
+        }
+
+    }
+
+void del(){
+    if(size==0){
+        cout<<"NOTHING TO DELETE "<<endl;
+        return ;
+    }
+    //STEP 1:-
+    arr[1]=arr[size];
+    //STEP 2:-
+    size--;  
+    //STEP 3:-
+
+    int i=1;
+    while(i<size){
+        int leftIndex=2*i;
+        int rightIndex=2*i + 1;
+
+        if(leftIndex< size && arr[i] < arr[leftIndex]){
+            swap(arr[i],arr[leftIndex]);
+            i=leftIndex;
+        }
+        else if(rightIndex < size && arr[i] < arr[rightIndex]){
+            swap(arr[i],arr[rightIndex]);
+            i=rightIndex;
+        }
+        else{
+            return ;
+        }
+    }
+}
+
+    void print(){
+        for(int i=1;i<=size;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+void heapify(int arr[],int n,int i){
+    int largest=i;
+    int left=2*i;
+    int right=2*i+1;
+
+    if(left < n && arr[largest] <arr[left]){
+        largest=left;
+    }
+    if(right< n && arr[largest] < arr[right]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+    }
+}
+
+int main(){
+    MinHeap h;
+    h.insert(10);
+    h.insert(20);
+    h.insert(30);
+    h.insert(40);
+    h.insert(50);
+    h.print();
+
+    return 0;
+}
