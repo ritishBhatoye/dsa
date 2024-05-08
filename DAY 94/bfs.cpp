65c7824bd601c66e534f21b873937e81a6b52dcf
Ritish Bhatoye <ritishrahul87@gmail.com>
2024-05-09 00:02:04 +0530

    DSA

--- DAY 94/bfs.cpp
+++ DAY 94/bfs.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
+#include<vector>
+#include<queue>
+
 using namespace std;
 
 
+class Sol{
+    public:
+    vector<int> bfsOfGraph(int V,vector<int> adj[]){
+        int vis[V]={0};
+        vis[0]=1;
 
+        queue<int> q;
+        q.push(0);
+
+        vector<int> bfs;
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+return bfs;
+    }
+};
 
 int main()
 {
