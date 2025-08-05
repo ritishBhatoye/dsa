3297a25492c6298529b371ea6d96bd9a9d1815c0
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-08-05 22:27:14 +0530

    secondLargestElement question

---
+++ DAY 3/5_Aug_2025/secondLargetElement.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void inputArr(int* arr,int size){
+    for(int i=0;i<size;i++)
+    {
+        cout<<"Enter element at "<<i<<" : ";
+        cin>>arr[i];
+    }
+}
+void printArr(int* arr,int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int secondLarget(int* arr,int size){
+    int max=arr[0];
+    int sMax=-1;
+
+    for(int i=0;i<size;i++){
+        if(arr[i]>max )
+        {
+            sMax=max;
+            max=arr[i];
+        }
+        else if(arr[i]<max && arr[i]>sMax){
+            sMax=arr[i];
+        }
+    }
+    return sMax;
+}
+
+int  main()
+{
+
+    int size;
+    cout<<endl;
+    cout<<"ENTER THE SIZE OF AN ARRAY : ";
+    cin>>size;
+    int* arr=new int[size];
+    cout<<endl;
+    inputArr(arr,size);
+    cout<<endl;
+    printArr(arr,size);
+    cout<<endl;
+    int result=secondLarget(arr,size);
+    cout<<"SECOND LARGEST ELEMENT "<<result<<endl;
+   
+    cout<<endl;
+    cout<<endl;
+
+    return 0;
+}
