98699ba7e6df55a99e78ee46c8e2f0dc11c86335
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-08-08 11:13:35 +0530

    sum-of-elements-of-array.cpp

---
+++ Strech-coding/Arrays/sumOfAnArray.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+void inputArr(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "Enter the element at " << i << " : ";
+        cin >> arr[i];
+    }
+}
+
+void printArr(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int sumOfArr(int* arr,int size){
+    int ans=0;
+    for(int i=0;i<size;i++)
+    {
+        ans=ans+arr[i];
+    }
+    return ans;
+}
+
+int main()
+{
+    int size;
+    cout<<endl<<endl;
+    cout<<"Enter the size of an array ";
+    cin>>size;
+    cout<<endl;
+    int* arr=new int[size];
+    inputArr(arr,size);
+    cout<<endl;
+    printArr(arr,size);
+    cout<<endl;
+
+    int result=sumOfArr(arr,size);
+    cout<<"SUM :- "<<result;
+    cout<<endl;
+
+    cout<<endl<<endl;
+
+    cout<<endl<<endl;
+
+
+    return 0;
+}
