7a93379e630ee7fbf4a507f219b2b2cd073ac4f4
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-08-15 14:04:52 +0530

    pair-sum

--- Strech-coding-weekend-1/pair_sum.cpp
+++ Strech-coding-weekend-1/pair_sum.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <cstdlib>
+#include<algorithm>
 #include<vector>
 
 using namespace std;
@@ -30,56 +31,80 @@ void printArr(int* arr,int size)
     }
 }
 
-void printVector(vector<int>& arr,int size){
+void printVector(const vector<int>& arr){
+    int size=arr.size();
     for(int i=0;i<size;i++)
     {
         cout<<arr[i]<<" ";
     }
 }
 
-vector<vector<<int>> pair_sum(vector<int>& arr,int size,int k)
+vector<vector<int>> pair_sum(vector<int>& arr,int size,int k)
 {
-    vector<int> ans;
+    sort(arr.begin(),arr.end());
+    vector<vector<int>> ans;
 
-    for(int i=0;i<size;)
+    int s=0;
+    int e=size-1;
+    while(s<e)
+    {
+        int sum=arr[s]+arr[e];
+
+        if(sum==k)
+        {
+            vector<int> temp;
+            temp.push_back(min(arr[s],arr[e]));
+            temp.push_back(max(arr[s],arr[e]));
+
+            ans.push_back(temp);
+
+            int leftVal=arr[s];
+            int rightVal=arr[e];
+
+            while(s<e && arr[s]==leftVal) s++;
+            while(s<e && arr[e]==rightVal) e--;
+        }
+        else if(sum < k){
+            s++;
+        }
+        else {
+            e--;
+        }
+    }
+return ans;
 
 }
 
 
 
-int main(){
-
-    int size1,size2;
+int main()
+{
+    int size;
     cout<<endl<<endl;
-    cout<<"Enter the size of an Array_1 :- ";
-    cin>>size1;
+    cout<<"Enter the size of array :- ";
+    cin>>size;
+    vector<int> arr(size);
     cout<<endl<<endl;
-    cout<<"Enter the size of an Array_2 :- ";
-    int* arr1=new int[size1];
-    cin>>size2;
-    cout<<endl<<endl;
-    cout<<"Input Elements in Array1"<<endl;
-    inputArr(arr1,size1);
-    int* arr2=new int[size2];
-
-    cout<<endl<<endl;
-
-    cout<<"Input Elements in Array2"<<endl;
-
-    inputArr(arr2,size2);
-
-    cout<<endl<<endl;
-
-
-
-    cout<<endl<<endl;
-    cout<<"Intersection of Arrays :- "<<endl;
-    array_intersection(arr1,arr2,size1,size2);
+    inputVector(arr,size);
     cout<<endl<<endl;
     
-    cout<<endl;
- 
+    printVector(arr);
+    
     cout<<endl<<endl;
+    int target;
+    cout<<"Enter the target value :- ";
+    cin>>target;
+    cout<<endl<<endl;
+
+    vector<vector<int>> ans=pair_sum(arr,size,target);
+
+    for(int i=0;i<ans.size();i++)
+    {
+        printVector(ans[i]);
+    }
+
+    cout<<endl<<endl;
+
 
     return 0;
 }
