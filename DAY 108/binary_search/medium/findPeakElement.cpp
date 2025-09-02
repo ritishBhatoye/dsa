af58413101575e515994961531c0313f8717c26e
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-09-02 22:04:58 +0530

    update launch configuration paths for binary search project

---
+++ DAY 108/binary_search/medium/findPeakElement.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include <utility> 
+
+using namespace std;
+
+ void inputArr(int* arr,int size)
+ {
+    for(int i=0;i<size;i++)
+    {
+        cout<<"Enter the element of an array at "<<i<<" ";
+        cin>>arr[i];
+    }
+ }
+
+void printArr(int* arr,int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int find_peak_element(int* arr,int size)
+{
+    int s=0;
+    int e=size-1;
+
+    while(s<e)
+    {
+        int mid=s+(e-s)/2;
+
+        if(arr[mid]<arr[mid+1])
+        {
+            s=mid+1;
+
+        }
+        else 
+        {
+            e=mid;
+        }
+
+    }
+    return s;
+}
+
+int main(){
+    cout<<endl;
+    int size;
+    cout<<"Enter the size of an array :- ";
+    cin>>size;
+    cout<<endl;
+    int* arr=new int[size];
+    inputArr(arr,size);
+    cout<<endl;
+    printArr(arr,size);
+    cout<<endl;
+  
+
+    cout<<endl<<endl;
+    int ans=find_peak_element(arr,size);
+    cout<<"Peak Element "<<arr[ans]<<" is at :- "<<ans;
+    cout<<endl<<endl;
+
+    cout<<endl;
+
+    return 0;
+}
---
+++ DAY 108/binary_search/medium/search_in_rotated_sorted_array.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include <utility> 
+
+using namespace std;
+
+ void inputArr(int* arr,int size)
+ {
+    for(int i=0;i<size;i++)
+    {
+        cout<<"Enter the element of an array at "<<i<<" ";
+        cin>>arr[i];
+    }
+ }
+
+void printArr(int* arr,int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int search_in_rotated_sorted_array(int*arr,int size,int k)
+{
+    int s=0;
+    int e=size-1;
+
+    while(s<=e)
+    {
+        int mid=s+(e-s)/2;
+
+        if(arr[mid]==k)
+        {
+            return mid;
+        }
+
+        if(arr[s]<=arr[mid])
+        {
+            if(arr[s]<=k && k<=arr[mid])
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+        else
+        {
+            if(arr[mid]<=k && k<=arr[e])
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        
+    }
+    return -1;
+}
+
+int main(){
+    cout<<endl;
+    int size;
+    cout<<"Enter the size of an array :- ";
+    cin>>size;
+    cout<<endl;
+    int* arr=new int[size];
+    inputArr(arr,size);
+    cout<<endl;
+    printArr(arr,size);
+    cout<<endl;
+    int k;
+    cout<<"Enter the element you want to search :- ";
+    cin>>k;
+
+    cout<<endl<<endl;
+    int ans=search_in_rotated_sorted_array(arr,size,k);
+    if(ans!=-1)
+    {
+        cout<<"Element "<<k<<" is present at :- "<<ans<<endl;
+    }
+    else
+    {
+        cout<<"Element "<<k<<" is not present "<<endl;
+    }
+    cout<<endl<<endl;
+
+    cout<<endl;
+
+    return 0;
+}
