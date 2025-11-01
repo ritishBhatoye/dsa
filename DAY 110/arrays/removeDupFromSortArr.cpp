c48b96a303a7f69344237501ef6f7f1c00a36d89
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-01 10:24:11 +0530

    remvoe_the_dup

--- DAY 110/arrays/removeDupFromSortArr.cpp
+++ DAY 110/arrays/removeDupFromSortArr.cpp
@@ -1,21 +1,38 @@
 #include<iostream>
 using namespace std;
 
-void removeDupFromSortArr(int *arr,int size)
+int removeDupFromSortArr(int *arr,int size)
 {
     int i=0;
     for(int j=1;j<size;j++)
     {
         if(arr[i]!=arr[j])
         {
-            
+            arr[i+1]=arr[j];
+            i++;
         }
+       
+    }
+    return i+1;
+}
+void printArr(int* arr,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
     }
 }
-
 int main()
 {
+   
+    int arr[6]={1,1,2,2,3,3};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int arr[5]={1,1,2,2,3,3};
+    cout<<endl<<endl;
+    int ans=removeDupFromSortArr(arr,size);
+    
+    cout<<endl<<endl;
+    printArr(arr,ans);
+    cout<<endl<<endl;
+
     return 0;
 }
