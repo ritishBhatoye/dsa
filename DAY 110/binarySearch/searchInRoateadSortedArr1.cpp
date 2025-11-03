80b20b58d87d92340d37596b586286e3120bebde
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-03 23:01:02 +0530

    SearchInRotatedSortedArr

--- DAY 110/binarySearch/searchInRoateadSortedArr1.cpp
+++ DAY 110/binarySearch/searchInRoateadSortedArr1.cpp
@@ -21,7 +21,7 @@ void printArr(vector<int> &arr,int size)
 }
 
 
-int firstOcc(vector<int>& arr,int k)
+int searchInRotatedSortedArr(vector<int>& arr,int k)
 {
     int s=0;
     int e=arr.size()-1;
@@ -33,52 +33,38 @@ int firstOcc(vector<int>& arr,int k)
 
         if(arr[mid]==k)
         {
-            ans=mid;
-            e=mid-1;
+            return mid;
+            break;
         }
-        else if(arr[mid] < k)
+
+
+        if(arr[s]<arr[mid])
         {
-            s=mid+1;
+            if(arr[s]<=k && k<=arr[mid])
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+                
+            }
         }
         else
         {
-            e=mid-1;
+            if(arr[mid] <=k && k<=arr[e])
+            {
+                s=mid-1;
+            }
+            else
+            {
+                e=mid-1;
+            }
         }
     }
 
     return ans;
 }
-
-
-int lastOcc(vector<int>& arr,int k)
-{
-    int s=0;
-    int e=arr.size()-1;
-    int ans=-1;
-
-    while(s<=e)
-    {
-        int mid=s+(e-s)/2;
-
-        if(arr[mid]==k)
-        {
-            ans=mid;
-            s=mid+1;
-        }
-        else if(arr[mid] < k)
-        {
-            s=mid+1;
-        }
-        else
-        {
-            e=mid-1;
-        }
-    }
-
-    return ans;
-}
-
-
 
 
 int main(){
@@ -93,15 +79,11 @@ int main(){
     inputArr(arr,size);
     printArr(arr,size);
     cout<<endl<<endl;
-    cout<<"Enter the element for which you want to find the First and Last Occurence :- ";
+    cout<<"Enter the element for which you want to find Position :- ";
     int k;
     cin>>k;
-    int firstOccAns=firstOcc(arr,k);
-    int lastOccAns=lastOcc(arr,k);
-    cout<<"First Ans :- "<<k<<" : "<<firstOccAns<<endl;
-    cout<<"Last Ans :- "<<k<<" : "<<lastOccAns<<endl;
-
-    cout<<"Total Occurence of an element :- "<<lastOccAns-firstOccAns+1<<endl;
+    int ans=searchInRotatedSortedArr(arr,k);
+    cout<<"SearchInRotatedSortedArr ANS :- "<<ans<<endl;
     cout<<endl<<endl;
 
     cout<<endl<<endl;
