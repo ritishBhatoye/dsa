61410bf415358057b341323aef47d262572e4a3e
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-03 23:28:12 +0530

    cntRotation for rotatedArr.cpp

---
+++ DAY 110/binarySearch/countRotationArr.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+void inputArr(vector<int> &arr,int size)
+{
+    for(int i=0;i<arr.size();i++)
+    {
+        cout<<"Enter the element at "<<i<<" : ";
+        cin>>arr[i];
+    }
+
+}
+
+void printArr(vector<int> &arr,int size)
+{
+    for(int i=0;i<arr.size();i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+int cntRotation(vector<int>& arr)
+{
+    int s=0;
+    int e=arr.size()-1;
+    int mini=INT_MAX;
+    int ans=-1;
+    
+    while(s<=e)
+    {
+        int mid=s+(e-s)/2;
+
+        if(arr[s] < arr[mid])
+        {
+            if(arr[s] < mini)
+            {
+                   mini=arr[s];
+                    ans=s;
+            }
+                s=mid+1;
+        }
+        else
+        {
+         
+            e=mid-1;
+        }
+    }
+
+    return ans;
+}
+
+
+int main(){
+
+    int size;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<"Enter the size of an array :- ";
+    cin>>size;
+    cout<<endl<<endl;
+    vector<int> arr(size);
+    inputArr(arr,size);
+    printArr(arr,size);
+    cout<<endl<<endl;
+    // cout<<"Enter the element for which you want to find Position :- ";
+    // int k;
+    // cin>>k;
+    int ans=cntRotation(arr);
+    cout<<"Total rotation :- "<<ans<<endl;
+    cout<<endl<<endl;
+
+    cout<<endl<<endl;
+
+
+    return 0;
+}
