5789c8be235c8238aa8f7abbd2b7a1636e89c5f1
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-09 13:04:05 +0530

    wavePrint

---
+++ DAY 112/2DArray/WavePrint.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void input2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<"Enter the element at row : "<<i<<" and at col : "<<j<<" = ";
+            cin>>arr[i][j];
+        }
+    }
+}
+
+void print2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl<<endl;
+    }
+}
+
+vector<int> wavePrintArray(vector<vector<int>> &arr,int row,int col)
+{   
+ vector<int> ans;
+for(int i=0;i<col;i++)
+{
+
+    if(i%2==0)
+    {
+
+
+        for(int j=0;j<row;j++)
+        {
+            ans.push_back(arr[j][i]);
+     
+        }
+    }
+    else
+    {
+ 
+
+        for(int j=row-1;j>=0;j--)
+        {
+
+
+           ans.push_back(arr[j][i]);
+        }
+    }
+    cout<<endl;
+}
+return ans;
+
+}
+
+int main()
+{
+
+    cout<<endl<<endl;
+    int row,col;
+    cout<<"Enter the number of rows :- ";
+    cin>>row;
+    cout<<"Enter the number of col :- ";
+    cin>>col;
+    cout<<endl<<endl;
+    vector<vector<int>> arr(row,vector<int>(col));
+    input2DArr(arr,row,col);
+    cout<<endl<<endl;
+    print2DArr(arr,row,col);
+    cout<<endl<<endl;
+
+   vector<int> ans= wavePrintArray(arr,row,col);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    
+    cout<<endl<<endl;
+
+    cout<<endl<<endl;
+
+
+    return 0;
+}
---
+++ DAY 112/2DArray/largestRowSum.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void input2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<"Enter the element at row : "<<i<<" and at col : "<<j<<" = ";
+            cin>>arr[i][j];
+        }
+    }
+}
+
+void print2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl<<endl;
+    }
+}
+
+int largestRowSum(vector<vector<int>> &arr,int row,int col)
+{
+    int ans=-1;
+    for(int i=0;i<row;i++)
+    {
+        int sum=0;
+        for(int j=0;j<col;j++)
+        {
+            sum+=arr[i][j];
+
+        }
+        if(ans < sum)
+        {
+            ans=sum;
+        }
+        sum=0;
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    cout<<endl<<endl;
+    int row,col;
+    cout<<"Enter the number of rows :- ";
+    cin>>row;
+    cout<<"Enter the number of col :- ";
+    cin>>col;
+    cout<<endl<<endl;
+    vector<vector<int>> arr(row,vector<int>(col));
+    input2DArr(arr,row,col);
+    cout<<endl<<endl;
+    print2DArr(arr,row,col);
+    cout<<endl<<endl;
+
+    int ans=largestRowSum(arr,row,col);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<"Largest Row Sum :-  "<<ans;
+    
+    cout<<endl<<endl;
+
+    cout<<endl<<endl;
+
+
+    return 0;
+}
