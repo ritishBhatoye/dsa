7db2b69a66f66b89a45711c19812200a78aa74e6
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-10 21:01:25 +0530

    few update

---
+++ DAY 113/2DArray/intro.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+void input2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<"Enter the element at row :- "<<i<<" and at col :- "<<j<<" = ";
+            cin>>arr[i][j];
+        }
+    }
+}
+
+void print2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    int row,col;
+    cout<<"Enter the row number :- ";
+    cin>>row;
+    cout<<"Enter the col number :- ";
+    cin>>col;
+    
+    cout<<endl<<endl;
+    vector<vector<int>> arr(row,vector<int>(col));
+    cout<<endl<<endl;
+    cout<<"Enter elements in to matrix :- ";
+    cout<<endl<<endl;
+    input2DArr(arr,row,col);
+    cout<<endl<<endl;
+    print2DArr(arr,row,col);
+    
+    cout<<endl<<endl;
+
+    return 0;
+}
