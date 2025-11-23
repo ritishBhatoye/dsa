61995cf45f185a4861f2c3dbe1b78fdd2112e225
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-23 14:17:12 +0530

    largestSumAndCol

---
+++ DAY 115/2D_Array/largetRowAndColSum.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+
+using namespace std;
+
+void input2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<"Enter the element at row :- "<<i<<" and at col :- "<<j<<" = ";
+            cin>>arr[i][j];
+        }
+    }
+}
+
+void print2DArr(vector<vector<int>> &arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int rowWiseSum(vector<vector<int>> &arr,int row,int col)
+{
+    int ans=0;
+    for(int i=0;i<row;i++)
+    {
+        int total=0;
+        for(int j=0;j<col;j++)
+        {
+            total+=arr[i][j];
+        }
+       if(total>ans)
+       {
+        ans=total;
+       }
+    }
+    return ans;
+}
+int colWiseSum(vector<vector<int>> &arr,int row,int col)
+{
+    int ans=0;
+    for(int i=0;i<col;i++)
+    {
+        int total=0;
+        for(int j=0;j<row;j++)
+        {
+            total+=arr[j][i];
+        }
+        if(ans < total)
+        {
+            ans=total;
+        }
+    }
+    return ans;
+}
+
+
+
+
+int main()
+{
+    cout<<endl<<endl;
+    int row,col;
+    cout<<"Enter the row :- ";
+    cin>>row;
+    cout<<"Enter the col :- ";
+    cin>>col;
+
+    vector<vector<int>> a(row,vector<int>(col));
+
+
+    cout<<endl<<endl;
+
+    cout<<endl<<endl;   
+    input2DArr(a,row,col);
+    cout<<endl<<endl;
+    cout<<"Print 2D Array :- "<<endl;
+    print2DArr(a,row,col);
+    cout<<endl<<endl;
+   
+    int rowAns=rowWiseSum(a,row,col);
+    cout<<"Row Wise Sum :- "<<rowAns<<endl;
+
+    cout<<endl<<endl;
+    // vector<vector<int>> productAns=matrixTranspose(a,row,col);
+    int colAns=colWiseSum(a,row,col);
+    cout<<"Col Wise Sum "<<colAns<<endl;
+    
+    cout<<endl<<endl;
+    // int ans=countNumberOfZero(a,row,col);
+    cout<<endl<<endl;
+    // cout<<"Total Count of numbers :- "<<ans<<endl;
+
+    cout<<endl<<endl;
+
+    return 0;
+}
