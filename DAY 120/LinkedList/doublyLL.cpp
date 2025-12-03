8aecbb32c07ad4fb8f96837300a26bdbf588d177
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-12-03 21:07:40 +0530

    update doublyLL

---
+++ DAY 120/LinkedList/doublyLL.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+
+void insertAtHead(Node* &head,int data)
+{
+    Node* temp= new Node(data);
+
+    head->prev=temp;
+    temp->next=head;
+    head=temp;
+    
+}
+
+void print(Node* &head)
+{
+    Node* temp=head;
+
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+void printReverse(Node* &head)
+{
+    Node* temp=head;
+
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->prev;
+    }
+}
+
+
+void insertAtTail(Node* &tail,int data)
+{
+    Node* temp=new Node(data);
+
+    tail->next=temp;
+    temp->prev=tail;
+    tail=temp;
+}
+
+void insertAtPostion(Node* &head,int data,int position)
+{
+    Node* temp=head;
+    Node* iNode=new Node(data);
+    int cnt=1;
+
+    while(cnt!=position-1)
+    {
+        temp=temp->next;
+        cnt++;
+    }
+
+    iNode->next=temp->next;
+    temp->next=iNode;
+    iNode->prev=temp;
+
+
+}
+
+int main()
+{
+    cout<<endl<<endl;
+    Node* n=new Node(10);
+    cout<<endl<<endl;
+    Node* head=n;
+    Node* tail=n;
+    insertAtTail(tail,20);
+    insertAtTail(tail,30);
+    insertAtTail(tail,40);
+    insertAtTail(tail,50);
+
+    cout<<endl<<endl;
+    print(head);
+    cout<<endl<<endl;
+    cout<<"Printing in the reverse order :- ";
+    printReverse(tail);
+
+    cout<<endl<<endl;
+    int k;
+    cout<<"Enter the position at which you want to insert the data :- ";
+    cin>>k;
+    int data;
+    cout<<"Enter the element you want to :- ";
+    cin>>data;
+    cout<<endl<<endl;
+    insertAtPostion(head,data,k);
+    cout<<endl<<endl;
+    print(head);
+    cout<<endl<<endl;
+
+    return 0;
+}
