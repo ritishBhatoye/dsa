8b41ca5be372d2613c0d63174687a01574632f5c
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-12-20 07:00:48 +0530

    few update

---
+++ DAY 126/Linked-List-Top-Questions/findMiddle.cpp
@@ -0,0 +1,138 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+
+};
+
+void inputAtHead(Node* &head,int data)
+{
+    Node* temp=new Node(data);
+
+    temp->next=head;
+    head=temp;
+}
+void inputAtTail(Node* &tail,int data)
+{
+    Node* temp=new Node(data);
+
+    tail->next=temp;
+    tail=temp;
+}
+
+Node* reverseLL(Node* &head)
+{
+
+    Node* curr=NULL;
+    Node* forw=head;
+
+    Node* temp=head;
+
+    while(forw!=NULL)
+    {
+        Node* temp=forw->next;
+
+        forw->next=curr;
+        curr=forw;
+        forw=temp;
+
+    }
+
+    return curr;
+}
+
+int length(Node* &head)
+{
+    Node* temp=head;
+
+    int cnt=0;
+
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        cnt++;
+    }
+
+    return cnt;
+}
+
+Node* findMiddle(Node* &head)
+{
+    Node* curr=head;
+    Node* forw=head;
+
+    int n=length(head);
+
+    if(n%2==0)
+    {
+    while(forw!=NULL)
+    {
+        forw=forw->next;
+        forw=forw->next;
+
+        curr=curr->next;
+    }
+    }
+    else
+    {
+    while(forw->next!=NULL)
+    {
+        forw=forw->next;
+        forw=forw->next;
+
+        curr=curr->next;
+    }
+    }
+
+        return curr;
+}
+
+
+void print(Node* &head)
+{
+
+    Node* temp=head;
+
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+
+    }
+}
+
+int main()
+{
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    Node* node=new Node(10);
+    Node* head=node;
+    Node* tail=node;
+    cout<<endl<<endl;
+    inputAtTail(tail,20);
+    inputAtTail(tail,30);
+    inputAtTail(tail,40);
+    inputAtTail(tail,50);
+    inputAtTail(tail,60);
+
+    print(head);
+    cout<<endl<<endl;
+    Node* ans=findMiddle(head);
+    cout<<endl<<endl;
+    print(ans);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+
+return 0;
+}
