a6ffd9ceb6d8007a03cc2e5127fc57b5681872a4
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-02-26 23:20:36 +0530

    auto: save 2026-02-26 23:20:36

--- DAY 130/LL_Easy/find_length.cpp
+++ DAY 130/LL_Easy/find_length.cpp
@@ -270,20 +270,25 @@ Node* mergeTwoSorted(Node* &l1,Node* &l2)
 
     while(head1 && head2)
     {
-        if(head1->data < head2->data)
+        if()
+        {
+
+        }
+        
+        else if(head1->data < head2->data)
         {
             Node* temp=new Node(head1->data);
 
             ansTail->next=temp;
             ansTail=ansTail->next;
         }
-        else
+        else if(head1->data > head2->data)
         {
             Node* temp=new Node(head2->data);
             
             ansTail->next=temp;
             ansTail=ansTail->next;
-            
+
         }
 
     }
