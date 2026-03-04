292fb72b22d92a6ee578ff3f16313dd60a281c2c
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-04 12:13:08 +0530

    tweak: DAY 131/easy

--- DAY 131/easy.cpp
+++ DAY 131/easy.cpp
@@ -52,7 +52,7 @@ int power(int a,int b)
    if(b==0)
         return 1;  
 
-    return a*power(a,b-1);    
+    return a * power(a,b-1);    
 }
 //Reverse an array / string using recursion
 void inputArr(int *arr,int size)
