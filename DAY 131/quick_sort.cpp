966ac7dee334dd06327132ac44ee1e7002255ba6
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-04 23:53:12 +0530

    fix: Improve inversion count logic in DAY 131/inversion_count.cpp

---
+++ DAY 131/quick_sort.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+
+void inputArr(int* arr,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<"Enter the element at "<<i<<" = ";
+        cin>>arr[i];
+    }
+}
+void printArr(int* arr,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int partition(int* arr,int s,int e)
+{
+    int pivot = arr[s];
+    int pivotIndex = 0;
+    int cnt=0;
+    for(int i=s+1 ; i<=e;i++)
+    {
+        if(pivot >= arr[i])
+        {
+            cnt++;
+        }
+    }
+    pivotIndex = s+ cnt;
+
+    swap(arr[s],arr[pivotIndex]);
+
+
+    int i=s,j=e;
+
+    while(i< pivotIndex && pivotIndex > j)
+    {
+        while(arr[i] <= pivot )
+        {
+            i++;
+        }
+        while(arr[j] > pivot)
+        {
+            j--;
+        }
+
+        if(i < pivotIndex && pivotIndex > j)
+        {
+            swap(arr[i++],arr[j--]);
+        }
+    }
+return pivotIndex;
+}
+
+void quickSort(int* arr,int s,int e)
+{
+    if(s>=e)
+    {
+        return ;
+    }
+
+    int p = partition(arr,s,e);
+
+
+    quickSort(arr,s,p-1);
+    quickSort(arr,p+1,e);
+
+}
+
+int main()
+{
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    int size;
+    cout<<"Enter the size of an array :- ";
+    cin>>size;
+
+    int* arr = new int[size];
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    inputArr(arr,size);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    printArr(arr,size);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    quickSort(arr,0,size-1);
+    cout<<"After applying quick sort :- ";
+    cout<<endl<<endl;
+
+    printArr(arr,size);
+
+    cout<<endl<<endl;
+
+    return 0;
+}
