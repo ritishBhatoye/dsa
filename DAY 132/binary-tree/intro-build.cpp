5deeb00462febd41c645d5f3377efc15ddbea6e4
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-05 22:25:36 +0530

    polish: DAY 132/binary-tree/intro-build

---
+++ DAY 132/binary-tree/intro-build.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+class node
+{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d)
+    {
+        this->data=d;
+        this->right=NULL;
+        this->left=NULL;
+    }
+
+};
+node* buildTree(node* root)
+{
+    cout<<"Enter the data :- ";
+    int d;
+    cin>>d;
+
+    root= new node(d);
+
+    if(d==-1)
+        return NULL;
+
+    cout<<"Enter the data in the LEFT of "<<d<<" :- "<<d<<endl;
+
+    root->left = buildTree(root->left);
+    
+    cout<<"Enter the data in the RIGHT of "<<d<<" :- "<<endl;
+    
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+void levelOrder(node* root)
+{
+    if(root==NULL)
+        return ;
+
+    queue<node*> q;
+
+    q.push(root);
+
+    while(!q.empty())
+    {
+        node* temp = q.front();
+
+        cout<<temp->data<<" ";
+
+        q.pop();
+
+        if(temp->left)
+        {
+            q.push(temp->left);
+        }
+        if(temp->right)
+        {
+            q.push(temp->right);
+        }
+    }
+
+}
+
+void preOrder(node* root)
+{
+    qu
+}
+
+int main()
+{
+
+    cout<<endl<<endl;
+    node* root=NULL;
+
+    root=buildTree(root);
+    cout<<endl<<endl;
+    cout<<"Printing level order traversal :- ";
+    cout<<endl<<endl;
+    levelOrder(root);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+
+    return 0;
+}
