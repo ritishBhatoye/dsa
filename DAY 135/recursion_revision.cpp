bf372e1d8182e579ffb891ccee30918287179d90
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-09 20:54:59 +0530

    refactor: streamline buildTreeFromLevelOrder function with enhanced error handling

---
+++ DAY 135/recursion_revision.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+// a ^ b
+int power(int a,int b)
+{
+    if(b==0)
+        return 1;
+
+    return a * power(a,b-1);
+
+}
+
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the Number :- ";
+    cin>>a;
+    cout<<"Enter the power :- ";
+    cin>>b;
+    cout<<endl<<endl;
+
+    int ans = power(a,b);
+    cout<<endl<<endl;
+    cout<<"Ans :- "<<ans<<endl;
+    cout<<endl<<endl;
+
+    return 0;
+}
