c5f3f211b0ff6389b7b2d5a7789488542eba8d85
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-09 23:21:48 +0530

    test: DAY 135/sort_using_recursion.cpp

--- DAY 135/sort_using_recursion.cpp
+++ DAY 135/sort_using_recursion.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
+
+void mergeSort(int s,int e,int* arr)
+{
+    if(s>e)
+        return ;
+
+    int mid = s + (e-s)/2; 
+    
+    //sorting the left portion
+    mergeSort(0,mid-1,arr);
+
+    //sorting the right portion
+    mergeSort(mid,e,)
+}
+
 int main()
 {
     return 0;
