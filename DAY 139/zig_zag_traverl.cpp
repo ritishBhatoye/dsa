fbe6b800b08811a1405f18bb9897ecee7022d368
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-14 13:36:18 +0530

    refactor: enhance readability and performance of isSumTree function in DAY 138/sum_tree.cpp

---
+++ DAY 139/zig_zag_traverl.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node* buildTree(node* root)
+{
+    cout<<"Enter the data :- ";
+    int d;
+    cin>>d;
+    
+    if(d==-1)
+        return NULL;
+
+    root = new node(d);
+
+    cout<<"Enter the data in the LEFT of "<<d<<endl;
+    root->left = buildTree(root->left);
+
+    cout<<"Enter the data in the RIGHT of "<<d<<endl;
+    root->right =  buildTree(root->right);
+
+    return root;
+}
+
+int height(node* root)
+{
+    if(root==NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right  = height(root->right);
+
+    return max(left,right) +1;
+}
+
+//LRN
+void postOrder(node* root)
+{
+    
+}
+
+//NLR
+void preOrder(node* root)
+{
+    if(root==NULL)
+        return ;
+
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+
+}
+
+void zigZagTraversal(node* root,int cnt)
+{
+
+    if(root==NULL)
+        return ;
+
+    if(cnt%2==0)
+    {
+        cout<<root->data<<" ";
+        zigZagTraversal(root->left,cnt+1);
+        zigZagTraversal(root->right,cnt+1);
+    }
+    
+    else
+    {
+        zigZagTraversal(root->left,cnt+1);
+        zigZagTraversal(root->right,cnt+1);
+        cout<<root->data<<" ";
+
+    }
+
+}
+
+
+
+int main()
+{
+    //    1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1 FALSE
+    // 3 1 -1 -1 2 -1 -1 TRUE
+    // 26 10 4 0 0 6 0 0 3 0 3 0 0
+    // 1 2 3 -1 -1 5 -1 -1 4 -1 6 7 -1 -1 8 -1 -1 
+    node* root = NULL;
+
+    root = buildTree(root);
+    cout<<endl<<endl;
+    zigZagTraversal(root,1);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+
+
+
+    return 0;
+}
