b611fc1540b50e8d31e98f0a8febee150685e3eb
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-17 20:30:36 +0530

    chore: DAY 140/diameter

--- DAY 140/diameter.cpp
+++ DAY 140/diameter.cpp
@@ -53,18 +53,18 @@ int height(node* root)
 int diameter(node* root)
 {
     if(root==NULL)
-        return ;
+        return 0;
 
     int op1 = diameter(root->left);
-    int op2 = diamter(root->right);
-    int op3 = height(root->right) + height(root->left);
+    int op2 = diameter(root->right);
+    int op3 = height(root->right) + height(root->left) +1;
 
     return max(op1,max(op2,op3));
 }
 
 int main()
 {
-    // 1 
+    // 1 2 4 8 -1 -1 -1 5 6 -1 7 -1 -1 -1 3 -1 -1 
     node* root = NULL;
     root = buildTree(root);
     cout<<endl<<endl;
