ef6055cc95ec83a7b39d051e932f0356c7590d60
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-08-16 19:53:41 +0530

    wip: DAY 151/STRECH CODING/Arrays/easy/moveAllNegToOneSide.cpp

---
+++ DAY 151/STRECH CODING/Arrays/easy/moveAllNegToOneSide.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void printArr(int* arr,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+int main()
+{
+    cout<<endl<<endl;
+
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+
+    return 0;
+}
