8ecbac1617000da51ee7dc703664e80cad89e324
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-08-16 21:16:00 +0530

    test: DAY 151/STRECH CODING/Arrays/easy/major.cpp

--- DAY 151/STRECH CODING/Arrays/easy/major.cpp
+++ DAY 151/STRECH CODING/Arrays/easy/major.cpp
@@ -3,9 +3,31 @@ using namespace std;
 
 
 void printArr(int* arr,int size)
-{}
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+void uniqueElement(int* arr,int size)
+{
+    for(int i=)
+}
+
 
 int main()
 {
+    cout<<endl<<endl;
+    int arr[7]={2,5,6,10,20,50,100};
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+
     return 0;
 }
