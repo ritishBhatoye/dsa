efb4c9f02f270fdd71c4345ea3698059108f93a0
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-08-17 19:06:24 +0530

    wip: DAY 151/STRECH CODING/Arrays/easy/major

--- DAY 151/STRECH CODING/Arrays/easy/major.cpp
+++ DAY 151/STRECH CODING/Arrays/easy/major.cpp
@@ -162,7 +162,7 @@ void sortArr(int* arr,int size)
 {
     for(int i=0;i<size-1;i++)
     {
-        for(int j=i;j<size-1;j++)
+        for(int j=0;j<size-1;j++)
         {
             if(arr[j]>arr[j+1])
             {
